3dc2b219f51db8090c3f0373a9146042551af204
Tommaso Morganti <tommaso.morganti@mail.polimi.it>
2022-04-05 01:12:34 +0200

    added links to documentation

--- App.tsx
+++ App.tsx
@@ -1,28 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 
-function Saluto(props: { nome: string }) {
+// I componenti sono delle funzioni, a cui viene passato come argomento un oggetto props
+// https://it.reactjs.org/docs/components-and-props.html
+// possono essere riutilizzati (tipo bottoni stilizzati) o possono rappresentare intere pagine
+let Saluto: FC<{ nome: string }> = props => {
     return <Text>Ciao {props.nome}</Text>
 }
 
 export default function App() {
+    // le variabili nella UI possono essere gestite con lo stato di react
+    // https://it.reactjs.org/docs/state-and-lifecycle.html
     let [nome, setNome] = useState('Tommaso')
 
-    useEffect((): void => {
-        fetch('https://jsonplaceholder.typicode.com/todos/1').then(response => response.text()).then(res =>
-            console.log(res)
-        )
+    useEffect(() => {
+        // useEffect può essere usato per chiamare del codice quando i props o lo stato cambiano
+        // https://it.reactjs.org/docs/hooks-effect.html
+
+        // per chiamate a servizi web, si può usare fetch nativo di Javascript
+        // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+        // potenzialmente anche altre librerie di terze parti possono essere usate 
+        // https://reactnative.dev/docs/network
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+            .then(async res => {
+                let text = await res.json()
+                console.log(text)
+            })
+        // l'array vuoto passato come secondo argomento indica che il useEffect deve essere eseguito
+        // solo alla prima renderizzazione
     }, [])
 
     return (
         <View style={styles.container}>
-            <Saluto nome={nome} />
+
+            <Saluto
+                // i componenti vengono messi nella UI con una sintassi simile ai tag HTML
+                // si possono passare dei props per cambiare cosa succede nel componente
+                nome={nome}
+            />
+
             <Pressable onPress={() => {
+                // chiamata quando viene premuto il bottone, aggiorna lo state
                 setNome('Giovanni')
             }} >
-                <Text>Cambia nome</Text>
+                <Text style={{
+                    // stili possono essere passati come prop, come un oggetto plain o usando la
+                    // funzione StyleSheet.create
+                    margin: 10,
+                    padding: 8,
+                    backgroundColor: '#f0f0f0',
+                }}> Cambia nome </Text>
             </Pressable>
+
             <StatusBar style="auto" />
         </View>
     );
---
+++ README.md
@@ -0,0 +1,51 @@
+# Polifemo
+
+### Setup dell'ambiente
+per usare 
+- [NodeJS](http://nodejs.org/) versione lts 16.14.2 (che su linux/mac consiglio di installare 
+tramite [nvm](https://github.com/nvm-sh/nvm))
+- [Yarn](https://yarnpkg.com) (che con Node 16.10+ si può attivare con il comando ```corepack enable```)
+- La CLI Expo che si può installare con il comando 
+    ```sh
+    npm install --global expo-cli
+    ```
+- L'applicazione Expo GO installata sul proprio telefono (https://expo.dev/client)
+
+### Esecuzione
+Una volta clonata la repo la prima cosa da fare è installare le dependencies con
+```sh
+yarn
+```
+per la prima installazione ci vorrà una vita perché le dev dependencies di react native sono più
+pesanti di tua mamma, ma solo la prima volta
+
+Poi c'è anche un bug in vscode alla prima installazione, a quanto pare bisogna chiudere e riaprire 
+la finestra perché non si rende conto che le dependencies sono state installate
+
+L'app può essere eseguita sul dispositivo con il comando
+```sh
+yarn start
+```
+e inquadrando il codice QR che viene sputato nella linea di comando, e il bundle javascript verrà
+scaricato nell'app Expo Go, che supporta hot-reload e da cui si può aprire un element inspector e
+avviare debugger scuotendo con molta rabbia e violenza il telefono
+
+Più informazioni riguardo ai comandi nei docs della [Expo CLI](https://docs.expo.dev/workflow/expo-cli/)
+
+### debug
+va ancora impostato, comunque serve st'estenzione [React Native Tools](https://marketplace.visualstudio.com/items?itemName=msjsdiag.vscode-react-native)
+(che è nelle raccomandazioni)
+
+### Risorse
+Consigliato dare un'occhiata alla [demo di autenticazione con expo](https://github.com/toto04/poliauth-expo-demo)
+per vedere come può essere strutturata un'app
+
+Molte risorse utili sono linkate nei commenti in [App.tsx](App.tsx)
+
+In generale è sono scritte molto bene le documentazioni di 
+- [React](https://it.reactjs.org/docs/getting-started.html)
+per la sintassi e i paradigmi utilizzati per la UI
+- [React Native](https://reactnative.dev/docs/getting-started)
+specificatamente su come funziona su mobile e
+- [Expo](https://docs.expo.dev) per le molti moduli nativi
+(tipo fotocamera, gps, ecc.)
