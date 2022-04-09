b9431501ade0c17c87c24adf18a728c2df2c7182
Tommaso Morganti <tommaso.morganti01@gmail.com>
2022-04-09 22:02:57 +0200

    added linting

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["react", "@typescript-eslint", "prettier"],
+    rules: {
+        "no-extra-parens": [
+            "error",
+            "all",
+            { ignoreJSX: "single-line", returnAssign: false },
+        ],
+        indent: ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "double"],
+        semi: ["error", "never"],
+    },
+}
