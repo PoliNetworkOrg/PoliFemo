4e1bbdaa2be99a11fc2481975e37d449bb6b8152
Tommaso Morganti <tommaso.morganti01@gmail.com>
2022-05-10 00:38:07 +0200

    repaired package versions

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
-import { NavigationContainer } from "@react-navigation/native"
 import React from "react"
+import { NavigationContainer } from "@react-navigation/native"
 import { RootStack } from "./src/navigation/RootStackNavigator"
 
+// eslint-disable-next-line @typescript-eslint/naming-convention
 export default function App() {
     return (
         <NavigationContainer>
