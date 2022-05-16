74ba7961e4564ab8adda3326cdbeec45f9d282a7
Pontig <pontiggiaelia@gmail.com>
2022-05-16 23:39:25 +0200

    now the onDayPres does work even on android

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,16 +1,10 @@
 import React, { useState } from "react"
-import {
-    RefreshControl,
-    ScrollView,
-    StyleSheet,
-    Text,
-    View,
-} from "react-native"
-import { Calendar, LocaleConfig, CalendarList } from "react-native-calendars"
+import { RefreshControl, ScrollView } from "react-native"
+import { Calendar, LocaleConfig } from "react-native-calendars"
 import { RootStackScreen } from "../navigation/NavigationTypes"
 import { formatDate } from "../util"
 
-const colors = ["#726FBF", "#DBD5F2"]
+const colors = ["#aadff2"]
 
 LocaleConfig.locales["it"] = {
     monthNames: [
@@ -51,9 +45,12 @@ LocaleConfig.locales["it"] = {
         "Sabato",
     ],
     dayNamesShort: ["Dom.", "Lun.", "Mar.", "Mer.", "Gio.", "Ven.", "Sab."],
-    today: "Oggi",
 }
 LocaleConfig.defaultLocale = "it"
+
+const onceDayIsPressed = (day: string): void => {
+    console.log(`Day ${day} is pressed`)
+}
 
 export const CalendarPage: RootStackScreen<"Calendar"> = ({ navigation }) => {
     const today = new Date()
@@ -76,40 +73,77 @@ export const CalendarPage: RootStackScreen<"Calendar"> = ({ navigation }) => {
                 />
             }
         >
-            <CalendarList
-                current={selected}
-                firstDay={1}
-                markedDates={{}}
+            <Calendar
+                current={minimum}
                 minDate={minimum}
-                onDayPress={day => {
-                    setSelected(day.dateString)
-                }}
-                markingType={"multi-dot"}
-                hideExtraDays={false}
-                disableArrowLeft={true}
-                disableAllTouchEventsForDisabledDays={false}
-                pastScrollRange={0}
-                scrollEnabled={true}
-                showScrollIndicator={false}
-                horizontal={true}
-                pagingEnabled={true}
+                onDayPress={day => onceDayIsPressed(day.dateString)}
+                //onDayLongPress={day => onceDayIsPressed(day.dateString)} //NON RIMOUOVERE QUESTA LINEA PER NESSUN CAZZO DI MOTIVO
+                hideArrows={true}
+                hideExtraDays={true}
+                firstDay={7}
+                enableSwipeMonths={true} //TODO: da rimuovere
                 theme={{
-                    todayTextColor: colors[1],
+                    backgroundColor: "#ffffff",
                     calendarBackground: colors[0],
+                    textSectionTitleColor: "#b6c1cd",
+                    textSectionTitleDisabledColor: "#d9e1e8",
+                    selectedDayBackgroundColor: "#00adf5",
+                    selectedDayTextColor: "#ffffff",
+                    todayTextColor: "#ff00ff",
                     dayTextColor: "#fff",
-                    textDisabledColor: "#aaa",
-                    monthTextColor: "#fff",
-                }}
-                style={{
-                    borderBottomWidth: 2,
-                    borderBottomColor: "#fff",
-                    shadowColor: "#000",
-                    shadowOffset: {
-                        width: 0,
-                        height: 4,
+                    textDisabledColor: "#d9e1e8",
+                    dotColor: "#00adf5",
+                    selectedDotColor: "#ffffff",
+                    arrowColor: "orange",
+                    disabledArrowColor: "#d9e1e8",
+                    monthTextColor: "blue",
+                    indicatorColor: "blue",
+                    textDayFontFamily: "monospace",
+                    textMonthFontFamily: "monospace",
+                    textDayHeaderFontFamily: "monospace",
+                    textDayFontWeight: "300",
+                    textMonthFontWeight: "bold",
+                    textDayHeaderFontWeight: "300",
+                    textDayFontSize: 16,
+                    textMonthFontSize: 16,
+                    textDayHeaderFontSize: 16,
+                }} //TODO: la gestione centralizzata dei colori
+                markingType="multi-period"
+                markedDates={{
+                    "2022-05-16": {
+                        periods: [
+                            {
+                                startingDay: false,
+                                endingDay: true,
+                                color: "#5f9ea0",
+                            },
+                            {
+                                startingDay: false,
+                                endingDay: true,
+                                color: "#ffa500",
+                            },
+                            {
+                                startingDay: true,
+                                endingDay: false,
+                                color: "#f0e68c",
+                            },
+                        ],
                     },
-                    shadowOpacity: 0.15,
-                    shadowRadius: 6,
+                    "2022-05-19": {
+                        periods: [
+                            {
+                                startingDay: true,
+                                endingDay: false,
+                                color: "#ffa500",
+                            },
+                            { color: "transparent" },
+                            {
+                                startingDay: false,
+                                endingDay: false,
+                                color: "#f0e68c",
+                            },
+                        ],
+                    },
                 }}
             />
         </ScrollView>
