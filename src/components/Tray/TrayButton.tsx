bbf96468175172f6f80fce9a3bd408a6aba50bfd
Cosimo Negri <cosimogiovanninegri@gmail.com>
2022-10-22 11:12:04 +0200

    CU-31f1bqj - Aggiornare le icone SVG del tray

--- src/components/Tray/TrayButton.tsx
+++ src/components/Tray/TrayButton.tsx
@@ -1,17 +1,21 @@
 import React, { FC } from "react"
 import { Pressable } from "react-native"
+import { Canvas, ImageSVG, useSVG } from "@shopify/react-native-skia"
 
-import { usePalette } from "utils/colors"
-import { SettingsSVG } from "assets/tray/Settings"
-import { NotificationsSVG } from "assets/tray/Notifications"
-import { DownloadsSVG } from "assets/tray/Downloads"
+// import { usePalette } from "utils/colors"
+import { trayIcons } from "assets/tray"
 
 export const TrayButton: FC<{
     onClick: () => void
-    label: "settings" | "notifications" | "downloads"
+    label: "downloads" | "notifications" | "settings"
 }> = props => {
-    const { palette } = usePalette()
-    const color = palette.lighter
+    // const { palette } = usePalette()
+    // const color = palette.lighter
+
+    const downloadsSVG = useSVG(trayIcons.downloads.svg)
+    const notificationsSVG = useSVG(trayIcons.notifications.svg)
+    const settingsSVG = useSVG(trayIcons.settings.svg)
+
     return (
         <Pressable
             onPress={props.onClick}
@@ -20,12 +24,57 @@ export const TrayButton: FC<{
                 marginLeft: 0,
             }}
         >
-            {props.label === "settings" ? (
-                <SettingsSVG color={color} />
+            {props.label === "downloads" ? (
+                <Canvas
+                    style={{
+                        width: trayIcons.downloads.width,
+                        height: trayIcons.downloads.heigth,
+                    }}
+                >
+                    {downloadsSVG && (
+                        <ImageSVG
+                            svg={downloadsSVG}
+                            x={0}
+                            y={0}
+                            width={trayIcons.downloads.width}
+                            height={trayIcons.downloads.heigth}
+                        />
+                    )}
+                </Canvas>
             ) : props.label === "notifications" ? (
-                <NotificationsSVG color={color} />
+                <Canvas
+                    style={{
+                        width: trayIcons.notifications.width,
+                        height: trayIcons.notifications.heigth,
+                    }}
+                >
+                    {notificationsSVG && (
+                        <ImageSVG
+                            svg={notificationsSVG}
+                            x={0}
+                            y={0}
+                            width={trayIcons.notifications.width}
+                            height={trayIcons.notifications.heigth}
+                        />
+                    )}
+                </Canvas>
             ) : (
-                <DownloadsSVG color={color} />
+                <Canvas
+                    style={{
+                        width: trayIcons.settings.width,
+                        height: trayIcons.settings.heigth,
+                    }}
+                >
+                    {settingsSVG && (
+                        <ImageSVG
+                            svg={settingsSVG}
+                            x={0}
+                            y={0}
+                            width={trayIcons.settings.width}
+                            height={trayIcons.settings.heigth}
+                        />
+                    )}
+                </Canvas>
             )}
         </Pressable>
     )
