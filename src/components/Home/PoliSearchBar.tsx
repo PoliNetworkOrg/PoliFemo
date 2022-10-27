01ba4812a14f29ae3d4b1796ece4b105abeb30c8
AndreaTorti-01 <eliandrea.yt@gmail.com>
2022-10-27 23:17:29 +0200

    improved SB look

--- src/components/Home/PoliSearchBar.tsx
+++ src/components/Home/PoliSearchBar.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react"
-import { TextInput } from "react-native"
+import { TextInput, View } from "react-native"
 import { usePalette } from "utils/colors"
 
 /**
@@ -8,16 +8,29 @@ import { usePalette } from "utils/colors"
 export const PoliSearchBar: FC = () => {
     const { palette } = usePalette()
     return (
-        <TextInput
-            autoCorrect={true}
-            placeholder="polifemo"
-            textAlign="center"
+        <View
             style={{
-                backgroundColor: palette.lighter,
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
+                marginVertical: 46,
+                marginHorizontal: 52,
+                borderRadius: 28,
+                borderColor: palette.darker,
+                borderStyle: "solid",
+                borderWidth: 1,
             }}
-        />
+        >
+            <View
+                style={{
+                    marginVertical: 10,
+                    marginLeft: 18,
+                    flexDirection: "row",
+                }}
+            >
+                <TextInput
+                    autoCorrect={true}
+                    placeholder="Cerca"
+                    selectionColor={palette.darker}
+                />
+            </View>
+        </View>
     )
 }
