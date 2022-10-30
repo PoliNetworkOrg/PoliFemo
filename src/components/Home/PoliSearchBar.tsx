04b4a21d78f1250101df75b6170043112c781dd3
AndreaTorti-01 <eliandrea.yt@gmail.com>
2022-10-30 15:20:09 +0100

    now clickable and correctly displayed

--- src/components/Home/PoliSearchBar.tsx
+++ src/components/Home/PoliSearchBar.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react"
-import { TextInput, View } from "react-native"
+import { TextInput, View, Pressable } from "react-native"
 import { usePalette } from "utils/colors"
 import { Canvas, ImageSVG, useSVG } from "@shopify/react-native-skia"
 import icon from "assets/menu/search.svg"
@@ -7,9 +7,12 @@ import icon from "assets/menu/search.svg"
 /**
  * butterblablabla
  */
-export const PoliSearchBar: FC = () => {
+export const PoliSearchBar: FC<{
+    onPress: () => void
+}> = ({ onPress }) => {
     const { palette } = usePalette()
     const svg = useSVG(icon)
+    const color = palette.primary
     return (
         <View
             style={{
@@ -17,34 +20,48 @@ export const PoliSearchBar: FC = () => {
                 marginHorizontal: 52,
                 borderRadius: 28,
                 backgroundColor: "#F6F7FC",
+                justifyContent: "space-between",
+                flexDirection: "row",
             }}
         >
+            <TextInput
+                style={{
+                    paddingVertical: 10,
+                    paddingLeft: 18,
+                    color: color,
+                }}
+                autoCorrect={true}
+                placeholder="Cerca"
+                selectionColor={color}
+                maxLength={32}
+                onSubmitEditing={onPress}
+            />
             <View
                 style={{
-                    marginVertical: 10,
-                    marginHorizontal: 18,
-                    flexDirection: "row",
+                    paddingVertical: 10,
+                    paddingRight: 18,
                     alignItems: "center",
-                    justifyContent: "space-between",
+                    justifyContent: "center",
                 }}
             >
-                <TextInput
-                    style={{ width: "90%" }}
-                    autoCorrect={true}
-                    placeholder="Cerca"
-                    selectionColor={palette.primary}
-                />
-                <Canvas style={{ flex: 1, width: 22, height: 22 }}>
-                    {svg && (
-                        <ImageSVG
-                            svg={svg}
-                            x={0}
-                            y={0}
-                            width={22}
-                            height={22}
-                        />
-                    )}
-                </Canvas>
+                <Pressable onPress={onPress}>
+                    <Canvas
+                        style={{
+                            width: 22,
+                            height: 22,
+                        }}
+                    >
+                        {svg && (
+                            <ImageSVG
+                                svg={svg}
+                                x={0}
+                                y={0}
+                                width={22}
+                                height={22}
+                            />
+                        )}
+                    </Canvas>
+                </Pressable>
             </View>
         </View>
     )
