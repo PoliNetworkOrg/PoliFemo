0680439b60bcf8b0c6abb04da9b94c523d7e3fbf
Cosimo Negri <cosimogiovanninegri@gmail.com>
2022-11-30 18:07:48 +0100

    CU-332dk6u - Creare componenti per sezione news
    - Use async/await instead of then/catch
    - Store list of articles and categories objects instead of ReactNodes
    - minor name fix

--- src/components/Home/NewsCategoriesGrid.tsx
+++ src/components/Home/NewsCategoriesGrid.tsx
@@ -1,16 +1,19 @@
-import React, { useState, useEffect, ReactNode } from "react"
+import React, { useState, useEffect } from "react"
 import { View } from "react-native"
 
 import { CardWithGradient } from "components/CardWithGradient"
-import { api } from "api"
+import { api, Tag } from "api"
 import { useNavigation } from "navigation/NavigationTypes"
 import { newsCategoryPatterns, CardsPattern } from "utils/cardsPatterns"
 
-export interface NewsCategoryCards {
-    /** left column of news category cards*/
-    left: ReactNode[]
-    /** right column of news category cards*/
-    right: ReactNode[]
+/** Tuple contianing the category object with the height of its card */
+export type CategoryAndHeight = [Tag, number]
+
+export interface CategoriesColumns {
+    /** left column of news categories*/
+    left: CategoryAndHeight[]
+    /** right column of news categories*/
+    right: CategoryAndHeight[]
 }
 
 /**
@@ -20,105 +23,120 @@ export interface NewsCategoryCards {
 export const NewsCategoriesGrid = () => {
     const navigation = useNavigation()
 
-    // Store the list of news category cards divided into left and right columns
-    const [cards, setCards] = useState<NewsCategoryCards>({
+    // Store the list of news categories with their heights, divided into left and right columns
+    const [categories, setCategories] = useState<CategoriesColumns>({
         left: [],
         right: [],
     })
 
-    // TODO: should add a way to refresh news categories ?
-    useEffect(() => {
-        // Get the news categories from the backend and build the list of cards
-        // using hardcoded patterns to get heights and positions (left / right) of the cards
-        api.getTags()
-            .then(categories => {
-                const tempCards: NewsCategoryCards = { left: [], right: [] }
+    // Get the news categories from the backend and use hardcoded
+    // patterns to get heights and positions (left / right) of the cards
+    const updateNewsCategories = async () => {
+        try {
+            const response = await api.getTags()
+            const tempCategories: CategoriesColumns = { left: [], right: [] }
 
-                // store the pattern data of the current batch of cards
-                let pattern: CardsPattern
-                // index of the current category
-                let index = 0
-                // number of categories remaining
-                let remaining = categories.length
+            // store the pattern data of the current batch of cards
+            let pattern: CardsPattern
+            // index of the current category
+            let index = 0
+            // number of categories remaining
+            let remaining = response.length
 
-                while (index < categories.length) {
-                    // choose the dimension of the next batch of cards and get the corresponding pattern
-                    if (index === 0) {
-                        // When first bacth
-                        if (remaining <= 6) {
-                            pattern = newsCategoryPatterns.first[remaining]
-                        } else {
-                            pattern = newsCategoryPatterns.first[4]
-                        }
+            while (index < response.length) {
+                // choose the dimension of the next batch of categories and get the corresponding pattern
+                if (index === 0) {
+                    // When first bacth
+                    if (remaining <= 6) {
+                        pattern = newsCategoryPatterns.first[remaining]
                     } else {
-                        // When other batches. Here it is never possible that remaining === 1 or remaining === 2
-                        if (remaining % 5 === 1 || remaining % 5 === 3) {
-                            pattern = newsCategoryPatterns.other[3]
-                        } else if (remaining % 5 === 2 || remaining % 5 === 4) {
-                            pattern = newsCategoryPatterns.other[4]
-                        } else {
-                            pattern = newsCategoryPatterns.other[5]
-                        }
+                        pattern = newsCategoryPatterns.first[4]
                     }
-                    // create all the cards using the data in the pattern and append them to the correct column
-                    for (const [height, column] of pattern) {
-                        const title = categories[index].name
-                        tempCards[column].push(
-                            <CardWithGradient
-                                key={index}
-                                title={title}
-                                imageURL={categories[index].image}
-                                onClick={() =>
-                                    navigation.navigate("NewsList", {
-                                        categoryName: title,
-                                    })
-                                }
-                                closerToCorner={true}
-                                style={{ height: height }}
-                            />
-                        )
-                        index++
-                        remaining--
+                } else {
+                    // When other batches. Here it is never possible that remaining === 1 or remaining === 2
+                    if (remaining % 5 === 1 || remaining % 5 === 3) {
+                        pattern = newsCategoryPatterns.other[3]
+                    } else if (remaining % 5 === 2 || remaining % 5 === 4) {
+                        pattern = newsCategoryPatterns.other[4]
+                    } else {
+                        pattern = newsCategoryPatterns.other[5]
                     }
                 }
-                setCards(tempCards)
-            })
-            .catch(error => console.log(error))
+
+                for (const [height, column] of pattern) {
+                    tempCategories[column].push([response[index], height])
+                    index++
+                    remaining--
+                }
+            }
+            setCategories(tempCategories)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    // TODO: should add a way to refresh news categories ?
+    useEffect(() => {
+        void updateNewsCategories()
     }, [])
+
+    // Function used when displaying the cards
+    const getCategoriesCards = (list: CategoryAndHeight[]) => {
+        return list.map(([category, height], index) => {
+            return (
+                <CardWithGradient
+                    key={index}
+                    title={category.name}
+                    imageURL={category.image}
+                    onClick={() =>
+                        navigation.navigate("NewsList", {
+                            categoryName: category.name,
+                        })
+                    }
+                    closerToCorner={true}
+                    style={{ height: height }}
+                />
+            )
+        })
+    }
 
     return (
         <View style={{ paddingBottom: 120 }}>
-            <CardWithGradient
-                // the news highlight card
-                title={"In Evidenza"}
-                imageURL={undefined} // TODO: update with correct highlight image
-                onClick={() =>
-                    navigation.navigate("NewsList", {
-                        categoryName: "In Evidenza",
-                    })
-                }
-                style={{ height: 220 }}
-            />
+            <>
+                <CardWithGradient
+                    // the news highlight card
+                    title={"In Evidenza"}
+                    imageURL={undefined} // TODO: update with correct highlight image
+                    onClick={() =>
+                        navigation.navigate("NewsList", {
+                            categoryName: "Evidenza",
+                        })
+                    }
+                    style={{ height: 220 }}
+                />
 
-            {cards.left.length === 1 && cards.right.length === 0 ? (
-                // if there is only 1 news category card, display it at full width
-                cards.left[0]
-            ) : (
-                <View style={{ flexDirection: "row" }}>
-                    <View
-                        // left column of news category cards
-                        style={{ flex: 17, marginRight: 17 }}
-                    >
-                        {cards.left}
+                {categories.left.length === 1 &&
+                categories.right.length === 0 ? (
+                    // if there is only 1 news category, display the card at full width
+                    categories.left[0]
+                ) : (
+                    <View style={{ flexDirection: "row" }}>
+                        <View
+                            // left column of news category cards
+                            style={{ flex: 17, marginRight: 17 }}
+                        >
+                            {getCategoriesCards(categories.left)}
+                        </View>
+
+                        <View
+                            // right column of news category cards
+                            style={{ flex: 14 }}
+                        >
+                            {getCategoriesCards(categories.right)}
+                        </View>
                     </View>
-                    <View
-                        // right column of news category cards
-                        style={{ flex: 14 }}
-                    >
-                        {cards.right}
-                    </View>
-                </View>
-            )}
+                )}
+            </>
         </View>
     )
 }
--- src/pages/NewsList.tsx
+++ src/pages/NewsList.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect, ReactNode } from "react"
+import React, { useState, useEffect } from "react"
 import { View } from "react-native"
 
-import { api, RetryType } from "api"
+import { api, Article, RetryType } from "api"
 import { RootStackScreen } from "navigation/NavigationTypes"
 import { Page } from "components/Page"
 import { CardWithGradient } from "components/CardWithGradient"
@@ -16,17 +16,42 @@ export const NewsList: RootStackScreen<"NewsList"> = props => {
     // State of the toggle switch in the header (on / off)
     const [toggled, setToggled] = useState<boolean>(false)
 
-    const [articlesCards, setArticlesCards] = useState<ReactNode[]>([])
+    const [articles, setArticles] = useState<Article[]>([])
 
     const [refreshing, setRefreshing] = useState<boolean>(false)
 
-    const updateArticles = (retryType: RetryType) => {
-        api.getArticles(retryType)
-            .then(articles => {
-                const tempArticlesCards: ReactNode[] = []
-                let index = 0
-                for (const article of articles) {
-                    tempArticlesCards.push(
+    // TODO: get only the articles of the given category
+    const updateArticles = async (retryType: RetryType) => {
+        try {
+            const response = await api.getArticles(retryType)
+            setArticles(response)
+        } catch (error) {
+            console.log(error)
+        }
+        setRefreshing(false)
+    }
+
+    useEffect(() => {
+        void updateArticles(RetryType.RETRY_INDEFINETELY)
+    }, [])
+
+    return (
+        <Page
+            title={categoryName}
+            refreshControl={{
+                refreshing,
+                onRefresh: () => {
+                    setRefreshing(true)
+                    void updateArticles(RetryType.NO_RETRY)
+                },
+            }}
+            showSwitch={true}
+            switchValue={toggled}
+            onSwitchToggle={value => setToggled(value)}
+        >
+            <View style={{ flex: 1, paddingTop: 16 }}>
+                {articles.map((article, index) => {
+                    return (
                         <CardWithGradient
                             key={index} // TODO: update with article id
                             title={article.title.it}
@@ -38,36 +63,8 @@ export const NewsList: RootStackScreen<"NewsList"> = props => {
                             style={{ height: 220, marginBottom: 13 }}
                         />
                     )
-                    index++
-                }
-                setArticlesCards(tempArticlesCards)
-            })
-            .catch(error => console.log(error))
-            .finally(() => {
-                setRefreshing(false)
-            })
-    }
-
-    // TODO: get only the articles of the given category
-    useEffect(() => {
-        updateArticles(RetryType.RETRY_INDEFINETELY)
-    }, [])
-
-    return (
-        <Page
-            title={categoryName}
-            refreshControl={{
-                refreshing,
-                onRefresh: () => {
-                    setRefreshing(true)
-                    updateArticles(RetryType.NO_RETRY)
-                },
-            }}
-            showSwitch={true}
-            switchValue={toggled}
-            onSwitchToggle={value => setToggled(value)}
-        >
-            <View style={{ flex: 1, paddingTop: 16 }}>{articlesCards}</View>
+                })}
+            </View>
         </Page>
     )
 }
