6ff8af442dc0c13df96eec832218206865bfda02
Diego Zafferani <43524162+DiegoZaff@users.noreply.github.com>
2022-12-11 12:10:28 +0100

    - ripple effect fixed maybe (?)

--- src/components/TouchableRipple.tsx
+++ src/components/TouchableRipple.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react"
-import { View, Pressable } from "react-native"
+import { View, TouchableNativeFeedback } from "react-native"
 import { usePalette } from "utils/colors"
 
 export interface TouchableRippleProps {
@@ -12,29 +12,28 @@ export const TouchableRipple: FC<TouchableRippleProps> = props => {
     const { isLight } = usePalette()
     return (
         <View
-            // ? per qualche motivo ignoto ponendo borderRadius = undefined nel caso in cui non
-            // ? ci debba essere borderRadius, il ripple effect fuorisce dalla view che wrappa
-            // ? il Pressable (effetto indesiderato).
-            // ? Sembra come se la View non si materializzi se non le viene dato
-            // ? un certo stile che la modifica (come il borderRadius oppure il backgroundColor).
-            // ? non sono sicuro di quest'ultima cosa, c'è anche la possibilità che abbia
-            // ? fatto un errore da qualche parte :(
-            style={{
-                overflow: "hidden",
-                borderTopLeftRadius: props.isRoundedTopCorners ? 30 : 0,
-                borderTopRightRadius: props.isRoundedTopCorners ? 30 : 0,
-            }}
+            style={[
+                {
+                    overflow: "hidden",
+                },
+                props.isRoundedTopCorners
+                    ? {
+                          borderTopLeftRadius: 30,
+                          borderTopRightRadius: 30,
+                      }
+                    : {},
+            ]}
         >
-            <Pressable
-                android_ripple={{
-                    color: isLight ? "#d2d2d2" : "#343E5A",
-                    borderless: true,
-                }}
+            <TouchableNativeFeedback
+                background={TouchableNativeFeedback.Ripple(
+                    isLight ? "#d2d2d2" : "#343E5A",
+                    false
+                )}
                 style={{}}
                 onPress={props.onClick ?? undefined}
             >
                 {props.children}
-            </Pressable>
+            </TouchableNativeFeedback>
         </View>
     )
 }
