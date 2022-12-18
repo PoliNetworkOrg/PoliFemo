2b00f714a7d08bb6875f33fee21343d6b45104a6
Cosimo Negri <cosimogiovanninegri@gmail.com>
2022-12-18 01:24:30 +0100

    From scrollView to flatlist

---
+++ src/components/ScrollPageInfinite.tsx
@@ -0,0 +1,244 @@
+import React, { FC, useEffect, useRef } from "react"
+import {
+    RefreshControl,
+    FlatList,
+    View,
+    Animated,
+    Switch,
+    StyleSheet,
+    NativeSyntheticEvent,
+    NativeScrollEvent,
+} from "react-native"
+
+import { Title, Subtitle } from "components/Text"
+import { NavBar, NavbarProps } from "components/NavBar"
+import { usePalette } from "utils/colors"
+
+import { Article } from "api"
+
+interface PageProps<T> {
+    /**
+     * Title in the sticky header.
+     */
+    title?: string
+    /**
+     * Subtitle in the sticky header.
+     */
+    subtitle?: string
+    /**
+     * Remove the navbar from the bottom of the page.
+     */
+    hideNavbar?: boolean
+    /**
+     * Props for the navbar, see {@link NavBar}
+     */
+    navbarOptions?: NavbarProps
+    /**
+     * Element to be put behind the page, the main content will scroll on top of it.
+     */
+    backdropElement?: React.ReactNode
+    /**
+     * Margin above the title to leave space for the backdrop element.
+     */
+    scrollOffset?: number
+    /**
+     * Whether or not to show a toggle switch on the right of the title.
+     *
+     * If true, provide also these 2 properties:
+     * - switchValue: boolean
+     * - onSwitchToggle: (value: booelan) => void
+     *
+     * @default false
+     */
+    showSwitch?: boolean
+    /**
+     * Used to draw the switch in the correct state (on / off).
+     */
+    switchValue?: boolean
+    /**
+     * Function fired when the switch changes its state (on / off).
+     */
+    onSwitchToggle?: (value: boolean) => void
+    /**
+     * props for the refresh control
+     */
+    refreshControl?: {
+        onRefresh: () => void
+        refreshing: boolean
+    }
+
+    data: T[]
+    render: (item: T) => React.ReactElement
+    fetchData: () => void
+
+    children?: React.ReactNode
+}
+
+/**
+ * General component useful for pages with an infinte scrollable content.
+ * It provides a navbar and a flatlist with margin and rounded corners.
+ */
+export const ScrollPageInfinite: FC<PageProps<Article>> = props => {
+    const { background, homeBackground, palette } = usePalette()
+    const [isPastTitle, setIsPastTitle] = React.useState(false)
+    const shadowAnim = useRef(new Animated.Value(0)).current
+
+    const navbar = !props.hideNavbar
+
+    const showHeader = props.title !== undefined
+
+    const showSwitch = props.showSwitch ?? false
+
+    useEffect(() => {
+        // hook called when the shadown needs to be animated
+        const duration = 100
+        if (isPastTitle)
+            Animated.timing(shadowAnim, {
+                toValue: 1,
+                duration,
+                useNativeDriver: true,
+            }).start()
+        else
+            Animated.timing(shadowAnim, {
+                toValue: 0,
+                duration,
+                useNativeDriver: true,
+            }).start()
+    }, [isPastTitle, shadowAnim])
+
+    const updatePastTitle = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
+        const scollThreshold = 20 + (props.scrollOffset || 0)
+        if (e.nativeEvent.contentOffset.y >= scollThreshold && !isPastTitle)
+            setIsPastTitle(true)
+        else if (e.nativeEvent.contentOffset.y < scollThreshold && isPastTitle)
+            setIsPastTitle(false)
+    }
+
+    return (
+        <View style={{ flex: 1, backgroundColor: homeBackground }}>
+            {props.backdropElement && (
+                // element at the top of the screen, above the page
+                <View
+                    style={{
+                        backgroundColor: homeBackground,
+                    }}
+                >
+                    {props.backdropElement}
+                </View>
+            )}
+            <View
+                // wrapper to make the borders of every child rounded
+                style={{
+                    backgroundColor: background,
+                    marginTop: 106 + (props.scrollOffset || 0),
+                    flex: 1,
+                    borderTopLeftRadius: 30,
+                    borderTopRightRadius: 30,
+                    overflow: "hidden",
+                    elevation: 15,
+                }}
+            >
+                <FlatList
+                    data={props.data}
+                    renderItem={({ item }) => props.render(item)}
+                    onEndReached={props.fetchData}
+                    refreshControl={
+                        props.refreshControl ? (
+                            <RefreshControl
+                                refreshing={props.refreshControl.refreshing}
+                                onRefresh={props.refreshControl.onRefresh}
+                            />
+                        ) : undefined
+                    }
+                    stickyHeaderIndices={props.title ? [0] : undefined} // first child is the sticky header
+                    scrollEventThrottle={100}
+                    onScroll={showHeader ? e => updatePastTitle(e) : undefined}
+                    ListHeaderComponent={
+                        showHeader ? (
+                            // Sticky page header with the title, subtitle and toggle switch
+                            <Animated.View
+                                style={[
+                                    styles.header,
+                                    {
+                                        backgroundColor: background,
+                                        shadowColor: isPastTitle
+                                            ? "#0003"
+                                            : undefined,
+                                        shadowOpacity: Animated.multiply(
+                                            shadowAnim,
+                                            0.27
+                                        ),
+                                        // this creates an unwanted shadow between the title and the content
+                                        // elevation: Animated.multiply(shadowAnim, 6),
+                                    },
+                                ]}
+                            >
+                                <View style={{ flexDirection: "row" }}>
+                                    <View>
+                                        <Title>{props.title}</Title>
+                                        {props.subtitle && (
+                                            <Subtitle>
+                                                {props.subtitle}
+                                            </Subtitle>
+                                        )}
+                                    </View>
+
+                                    {showSwitch && (
+                                        <Switch
+                                            value={props.switchValue}
+                                            onValueChange={value =>
+                                                props.onSwitchToggle &&
+                                                props.onSwitchToggle(value)
+                                            }
+                                            trackColor={{
+                                                false: homeBackground, // TODO: ask the design team which is the correct color
+                                                true: palette.accent,
+                                            }}
+                                            thumbColor={background}
+                                            style={styles.switch}
+                                        />
+                                    )}
+                                </View>
+                            </Animated.View>
+                        ) : undefined
+                    }
+                    contentContainerStyle={{
+                        backgroundColor: background,
+                        paddingTop: !showHeader ? 30 : 0,
+                        paddingBottom: 120,
+                    }}
+                />
+            </View>
+            {navbar ? <NavBar {...props.navbarOptions} /> : null}
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    header: {
+        zIndex: 1000,
+        paddingHorizontal: 28,
+        paddingVertical: 22,
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowRadius: 4.65,
+    },
+    backdrop: {
+        zIndex: -1,
+        height: 200,
+        width: "100%",
+        position: "absolute",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    switch: {
+        position: "absolute",
+        alignSelf: "center",
+        right: 8,
+        transform: [{ scaleX: 1.5 }, { scaleY: 1.5 }],
+    },
+})
--- src/pages/NewsList.tsx
+++ src/pages/NewsList.tsx
@@ -1,15 +1,17 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import { View } from "react-native"
 
 import { api, Article, RetryType } from "api"
-import { RootStackScreen } from "navigation/NavigationTypes"
-import { Page } from "components/Page"
+import { RootStackScreen, useNavigation } from "navigation/NavigationTypes"
+import { ScrollPageInfinite } from "components/ScrollPageInfinite"
 import { CardWithGradient } from "components/CardWithGradient"
 
 /**
  * News page containing the articles of a specific category.
  */
 export const NewsList: RootStackScreen<"NewsList"> = props => {
+    const navigation = useNavigation()
+
     // Name of the news category, which is the title of the page
     const { categoryName } = props.route.params
 
@@ -18,57 +20,87 @@ export const NewsList: RootStackScreen<"NewsList"> = props => {
 
     const [articles, setArticles] = useState<Article[]>([])
 
-    const [refreshing, setRefreshing] = useState<boolean>(false)
+    const [manualRefreshing, setManualRefreshing] = useState<boolean>(false)
+    const [automaticDownload, setAutomaticDownload] = useState<boolean>(false)
+
+    const lastDate = useRef<Date>(new Date())
 
     // TODO: get only the articles of the given category
-    const updateArticles = async (retryType: RetryType) => {
+    const updateArticles = async (retryType: RetryType, keep = true) => {
         try {
-            const response = await api.getArticlesFromDaysAgoTillDate(
-                3,
+            const response = await api.getArticlesFromDaysAgoTillDateByTag(
+                7,
                 new Date().toISOString(),
-                { retryType: retryType }
+                categoryName,
+                { retryType: retryType, maxRetries: 2, waitingTime: 2 }
             )
-            setArticles(response)
+
+            if (keep) {
+                setArticles([...articles, ...response.reverse()])
+            } else {
+                setArticles(response.reverse())
+            }
         } catch (error) {
             console.log(error)
         }
-        setRefreshing(false)
+        setManualRefreshing(false)
     }
 
     useEffect(() => {
-        void updateArticles(RetryType.RETRY_INDEFINETELY)
+        void updateArticles(RetryType.RETRY_N_TIMES)
     }, [])
 
     return (
-        <Page
+        <ScrollPageInfinite
             title={categoryName}
+            data={articles}
+            render={article => {
+                return (
+                    <View style={{ paddingHorizontal: 28 }}>
+                        <CardWithGradient
+                            key={article.id} // TODO: update with article id
+                            title={article.title}
+                            imageURL={article.image} // TODO: update with correct article image
+                            onClick={() =>
+                                navigation.navigate("Article", {
+                                    article: article,
+                                })
+                            }
+                            style={{ height: 220, marginBottom: 13 }}
+                        />
+                    </View>
+                )
+            }}
+            fetchData={() => {
+                void updateArticles(RetryType.NO_RETRY, false)
+            }}
             refreshControl={{
-                refreshing,
+                refreshing: manualRefreshing,
                 onRefresh: () => {
-                    setRefreshing(true)
-                    void updateArticles(RetryType.NO_RETRY)
+                    setManualRefreshing(true)
+                    void updateArticles(RetryType.NO_RETRY, false)
                 },
             }}
             showSwitch={true}
             switchValue={toggled}
             onSwitchToggle={value => setToggled(value)}
-        >
-            <View style={{ flex: 1, paddingTop: 16 }}>
-                {articles.map((article, index) => {
-                    return (
-                        <CardWithGradient
-                            key={index} // TODO: update with article id
-                            title={article.title}
-                            imageURL={undefined} // TODO: update with correct article image
-                            onClick={() => {
-                                // TODO: here navigate to the article page
-                                console.log(article.title)
-                            }}
-                            style={{ height: 220, marginBottom: 13 }}
-                        />
-                    )
-                })}
-            </View>
-        </Page>
+        />
+        // {/* <View style={{ flex: 1, paddingTop: 16 }}> ////////
+        //     {articles.map((article, index) => {
+        //         return (
+        //             <CardWithGradient
+        //                 key={index} // TODO: update with article id
+        //                 title={article.title}
+        //                 imageURL={article.image} // TODO: update with correct article image
+        //                 onClick={() =>
+        //                     navigation.navigate("Article", {
+        //                         article: article,
+        //                     })
+        //                 }
+        //                 style={{ height: 220, marginBottom: 13 }}
+        //             />
+        //         )
+        //     })}
+        // </View> */}
     )
 }
