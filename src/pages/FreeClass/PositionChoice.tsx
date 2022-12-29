a87879d0ca7b561d03d4a65614f4a0251f3c1cac
Matteo <matteo1.luppi@mail.polimi.it>
2022-12-29 17:30:42 +0100

    fix button state and graphic

--- src/pages/FreeClass/PositionChoice.tsx
+++ src/pages/FreeClass/PositionChoice.tsx
@@ -8,12 +8,18 @@ import { PoliSearchBar } from "components/Home"
 import PositionArrowIcon from "assets/freeClassrooms/positionArrow.svg"
 import { useSVG, Canvas, ImageSVG } from "@shopify/react-native-skia"
 
+enum ButtonType {
+    MAP,
+    LIST,
+}
+
 export const PositionChoice: RootStackScreen<"PositionChoice"> = () => {
     const [search, setSearch] = useState("")
     const { navigate } = useNavigation()
     const { homeBackground, background, primary } = usePalette()
-
     const positionArrowSVG = useSVG(PositionArrowIcon)
+
+    const [status, setStatus] = useState<ButtonType>(ButtonType.MAP)
 
     return (
         <View
@@ -63,7 +69,7 @@ export const PositionChoice: RootStackScreen<"PositionChoice"> = () => {
                                 style={{
                                     width: 25,
                                     height: 25,
-                                    backgroundColor: "white",
+                                    backgroundColor: background,
                                 }}
                             >
                                 <Canvas
@@ -102,26 +108,64 @@ export const PositionChoice: RootStackScreen<"PositionChoice"> = () => {
                     <View
                         style={{
                             marginTop: 22,
+                            display: "flex",
                             flexDirection: "row",
-                            alignSelf: "center",
+                            justifyContent: "center",
                         }}
                     >
                         <Pressable
                             style={{
                                 width: 134,
                                 height: 44,
-                                backgroundColor: primary,
+                                backgroundColor:
+                                    status === ButtonType.MAP
+                                        ? primary
+                                        : "#8791BD",
                                 borderRadius: 22,
+                                alignItems: "center",
+                                justifyContent: "center",
                             }}
-                        ></Pressable>
+                            onPress={() => setStatus(ButtonType.MAP)}
+                        >
+                            <BodyText
+                                style={{
+                                    fontWeight:
+                                        status === ButtonType.MAP
+                                            ? "900"
+                                            : "500",
+                                    color: "white",
+                                }}
+                            >
+                                Mappa
+                            </BodyText>
+                        </Pressable>
                         <Pressable
                             style={{
                                 width: 134,
                                 height: 44,
-                                backgroundColor: "#8791BD",
+                                backgroundColor:
+                                    status === ButtonType.LIST
+                                        ? primary
+                                        : "#8791BD",
                                 borderRadius: 22,
+                                marginLeft: 17,
+                                alignItems: "center",
+                                justifyContent: "center",
                             }}
-                        ></Pressable>
+                            onPress={() => setStatus(ButtonType.LIST)}
+                        >
+                            <BodyText
+                                style={{
+                                    fontWeight:
+                                        status === ButtonType.LIST
+                                            ? "900"
+                                            : "500",
+                                    color: "white",
+                                }}
+                            >
+                                Lista
+                            </BodyText>
+                        </Pressable>
                     </View>
                 </View>
             </View>
