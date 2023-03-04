0e90cce487990cc941691048aa1eedcc12451cd3
Tommaso Morganti <tommaso.morganti01@gmail.com>
2023-03-04 17:21:48 +0100

    fixed tray icons

--- src/components/Tray/TrayButton.tsx
+++ src/components/Tray/TrayButton.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react"
-import { Pressable } from "react-native"
+import { Pressable, StyleProp, ViewStyle } from "react-native"
 
 // import { usePalette } from "utils/colors"
 import { TrayIcon, trayIcons } from "assets/tray"
@@ -8,14 +8,18 @@ import { Icon } from "components/Icon"
 export const TrayButton: FC<{
   onClick: () => void
   label: TrayIcon
+  style?: StyleProp<ViewStyle>
 }> = props => {
   return (
     <Pressable
       onPress={props.onClick}
-      style={{
-        marginRight: 17,
-        marginLeft: 0,
-      }}
+      style={[
+        {
+          marginRight: 17,
+          marginLeft: 0,
+        },
+        props.style,
+      ]}
     >
       <Icon source={trayIcons[props.label]} />
     </Pressable>
