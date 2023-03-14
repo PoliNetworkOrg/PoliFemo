70186b7dd1c891915b3e548f03b6979397762584
Matteo Luppi <100372313+lups2000@users.noreply.github.com>
2023-03-14 08:50:15 +0100

    create first home strings

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -1,23 +1,23 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import i18n from "i18next"
 import { initReactI18next } from "react-i18next"
-import commonIT from "./jsons/it/common.json"
-import commonEN from "./jsons/en/common.json"
+import homeIT from "./jsons/it/home.json"
+import homeEN from "./jsons/en/home.json"
 import { getLocales } from "expo-localization"
 
 void i18n.use(initReactI18next).init({
   resources: {
     it: {
-      common: commonIT,
+      home: homeIT,
     },
     en: {
-      common: commonEN,
+      home: homeEN,
     },
   },
   lng: getLocales()[0].languageCode,
   compatibilityJSON: "v3",
   fallbackLng: "it",
-  defaultNS: "common",
+  defaultNS: "home",
   interpolation: {
     escapeValue: false,
   },
