9ef707cf04337484acdf48aa1065e6646dccbb73
github.nouser@nouser.github.com <github.nouser@nouser.github.com>
2023-03-16 18:02:19 +0100

    fix merge

--- src/components/Groups/FiltersList.tsx
+++ src/components/Groups/FiltersList.tsx
@@ -126,7 +126,7 @@ export const FiltersList: FC<FiltersProps> = props => {
           onPress={reset}
         />
       </View>
-      <ModalWithButtons
+      <Modal
         title={"" + t(getNameFromMode(modalMode))}
         centerText
         isShowing={isModalShowing}
