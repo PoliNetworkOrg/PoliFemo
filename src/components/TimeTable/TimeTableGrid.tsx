fba2e4e28e9a7bcba5e4365fd1ba20565dc8f6c8
DiegoZaff <diego.zaffe@gmail.com>
2023-05-01 15:09:36 +0200

    Color Picker is hidden by shadow

--- src/components/TimeTable/TimeTableGrid.tsx
+++ src/components/TimeTable/TimeTableGrid.tsx
@@ -24,245 +24,9 @@ import { Grid } from "./OverlayGrid"
 import { usePalette } from "utils/colors"
 import { ColorPickerLecture } from "./ColorPickerLecture"
 import { useFocusEffect } from "@react-navigation/native"
+import { TimetableBottomSheetHandle } from "./TimetableBottomSheetHandle"
 
 const { width } = Dimensions.get("window")
-
-const fakeLectures: Event[] = [
-  {
-    event_id: 127350,
-    date_start: "2023-04-10T08:00:00",
-    date_end: "2023-04-10T10:00:00",
-    show_agenda: true,
-    matricola: "100000",
-    title: {
-      it: "ARCHITETTURA DEI CALCOLATORI E SISTEMI OPERATIVI",
-      en: "COMPUTER ARCHITECTURES AND OPERATING SYSTEMS",
-    },
-    event_type: {
-      typeId: 1,
-      type_dn: { it: "Lezione", en: "Lecture" },
-    },
-    event_subtype: "L",
-    calendar: {
-      calendar_id: 0,
-      calendar_dn: { it: "Accademico", en: "Academic" },
-    },
-    room: {
-      room_id: 3316,
-      acronym_dn: "2.1.2",
-      classroom_id: -2147483648,
-      room_dn: "002",
-    },
-  },
-  {
-    event_id: 1251,
-    date_start: "2023-04-10T09:00:00",
-    date_end: "2023-04-10T11:00:00",
-    show_agenda: true,
-    matricola: "100000",
-    title: {
-      it: "INGEGNERIA DEL SOFTWARE",
-      en: "SOFTWARE ENGINEERING",
-    },
-    event_type: {
-      typeId: 1,
-      type_dn: { it: "Lezione", en: "Lecture" },
-    },
-    event_subtype: "L",
-    calendar: {
-      calendar_id: 0,
-      calendar_dn: { it: "Accademico", en: "Academic" },
-    },
-    room: {
-      room_id: 3316,
-      acronym_dn: "2.1.2",
-      classroom_id: -2147483648,
-      room_dn: "002",
-    },
-  },
-  {
-    event_id: 1231234,
-    date_start: "2023-04-10T10:00:00",
-    date_end: "2023-04-10T17:00:00",
-    show_agenda: true,
-    matricola: "100000",
-    title: {
-      it: "INGEGNERIA DEL SOFTWARE",
-      en: "SOFTWARE ENGINEERING",
-    },
-    event_type: {
-      typeId: 1,
-      type_dn: { it: "Lezione", en: "Lecture" },
-    },
-    event_subtype: "L",
-    calendar: {
-      calendar_id: 0,
-      calendar_dn: { it: "Accademico", en: "Academic" },
-    },
-    room: {
-      room_id: 3316,
-      acronym_dn: "2.1.2",
-      classroom_id: -2147483648,
-      room_dn: "002",
-    },
-  },
-  {
-    event_id: 12314,
-    date_start: "2023-04-11T10:00:00",
-    date_end: "2023-04-11T16:00:00",
-    show_agenda: true,
-    matricola: "100000",
-    title: {
-      it: "ARCHITETTURA DEI CALCOLATORI E SISTEMI OPERATIVI",
-      en: "COMPUTER ARCHITECTURES AND OPERATING SYSTEMS",
-    },
-    event_type: {
-      typeId: 1,
-      type_dn: { it: "Lezione", en: "Lecture" },
-    },
-    event_subtype: "L",
-    calendar: {
-      calendar_id: 0,
-      calendar_dn: { it: "Accademico", en: "Academic" },
-    },
-    room: {
-      room_id: 3316,
-      acronym_dn: "2.1.2",
-      classroom_id: -2147483648,
-      room_dn: "002",
-    },
-  },
-  {
-    event_id: 5868,
-    date_start: "2023-04-11T12:00:00",
-    date_end: "2023-04-11T18:00:00",
-    show_agenda: true,
-    matricola: "100000",
-    title: {
-      it: "INGEGNERIA DEL SOFTWARE",
-      en: "SOFTWARE ENGINEERING",
-    },
-    event_type: {
-      typeId: 1,
-      type_dn: { it: "Lezione", en: "Lecture" },
-    },
-    event_subtype: "L",
-    calendar: {
-      calendar_id: 0,
-      calendar_dn: { it: "Accademico", en: "Academic" },
-    },
-    room: {
-      room_id: 3316,
-      acronym_dn: "2.1.2",
-      classroom_id: -2147483648,
-      room_dn: "002",
-    },
-  },
-  {
-    event_id: 125115,
-    date_start: "2023-04-12T08:00:00",
-    date_end: "2023-04-12T10:00:00",
-    show_agenda: true,
-    matricola: "100000",
-    title: {
-      it: "INGEGNERIA DEL SOFTWARE",
-      en: "SOFTWARE ENGINEERING",
-    },
-    event_type: {
-      typeId: 1,
-      type_dn: { it: "Lezione", en: "Lecture" },
-    },
-    event_subtype: "L",
-    calendar: {
-      calendar_id: 0,
-      calendar_dn: { it: "Accademico", en: "Academic" },
-    },
-    room: {
-      room_id: 3316,
-      acronym_dn: "2.1.2",
-      classroom_id: -2147483648,
-      room_dn: "002",
-    },
-  },
-  {
-    event_id: 1353452,
-    date_start: "2023-04-12T09:00:00",
-    date_end: "2023-04-12T20:00:00",
-    show_agenda: true,
-    matricola: "100000",
-    title: {
-      it: "INGEGNERIA DEL SOFTWARE 2",
-      en: "SOFTWARE ENGINEERING",
-    },
-    event_type: {
-      typeId: 1,
-      type_dn: { it: "Lezione", en: "Lecture" },
-    },
-    event_subtype: "L",
-    calendar: {
-      calendar_id: 0,
-      calendar_dn: { it: "Accademico", en: "Academic" },
-    },
-    room: {
-      room_id: 3316,
-      acronym_dn: "2.1.2",
-      classroom_id: -2147483648,
-      room_dn: "002",
-    },
-  },
-  {
-    event_id: 1241551,
-    date_start: "2023-04-13T08:00:00",
-    date_end: "2023-04-13T19:00:00",
-    show_agenda: true,
-    matricola: "100000",
-    title: {
-      it: "INGEGNERIA DEL SOFTWARE 2",
-      en: "SOFTWARE ENGINEERING",
-    },
-    event_type: {
-      typeId: 1,
-      type_dn: { it: "Lezione", en: "Lecture" },
-    },
-    event_subtype: "L",
-    calendar: {
-      calendar_id: 0,
-      calendar_dn: { it: "Accademico", en: "Academic" },
-    },
-    room: {
-      room_id: 3316,
-      acronym_dn: "2.1.2",
-      classroom_id: -2147483648,
-      room_dn: "002",
-    },
-  },
-  {
-    event_id: 125151,
-    date_start: "2023-04-14T08:00:00",
-    date_end: "2023-04-14T11:00:00",
-    show_agenda: true,
-    matricola: "100000",
-    title: {
-      it: "INGEGNERIA DEL SOFTWARE 2",
-      en: "SOFTWARE ENGINEERING",
-    },
-    event_type: {
-      typeId: 1,
-      type_dn: { it: "Lezione", en: "Lecture" },
-    },
-    event_subtype: "L",
-    calendar: {
-      calendar_id: 0,
-      calendar_dn: { it: "Accademico", en: "Academic" },
-    },
-    room: {
-      room_id: 3316,
-      acronym_dn: "2.1.2",
-      classroom_id: -2147483648,
-      room_dn: "002",
-    },
-  },
-]
 
 // distance of the bottom sheet from the top of the screen, when opened or closed
 const distanceFromTop = {
@@ -289,7 +53,7 @@ export const TimeTableGrid: FC = () => {
     number | undefined
   >(undefined)
 
-  const { isLight, palette } = usePalette()
+  const { isLight, palette, background } = usePalette()
 
   const deducer = useRef<TimetableDeducer | undefined>()
 
@@ -402,13 +166,18 @@ export const TimeTableGrid: FC = () => {
       </View>
       <BottomSheet
         ref={bottomSheetRef}
+        handleComponent={TimetableBottomSheetHandle}
         index={-1}
         snapPoints={[getUsableScreenHeight() - distanceFromTop.closed]}
         enablePanDownToClose={true}
         onClose={() => setTimeTableOpen(true)}
+        backgroundStyle={{ backgroundColor: background }}
       >
         <BottomSheetScrollView
-          contentContainerStyle={{ marginHorizontal: 36, marginTop: 4 }}
+          contentContainerStyle={{
+            marginHorizontal: 36,
+            marginBottom: 100,
+          }}
         >
           <View
             style={{
