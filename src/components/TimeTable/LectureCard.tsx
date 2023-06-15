fb2a2c0f326048afbfbe7b32268499357081ee0c
Tommaso Morganti <tommaso.morganti01@gmail.com>
2023-06-15 17:22:04 +0200

    small style fix

--- src/components/TimeTable/LectureCard.tsx
+++ src/components/TimeTable/LectureCard.tsx
@@ -42,101 +42,72 @@ export const LectureCard: FC<LectureCardProps> = props => {
   const timeStart = new Date(props.lecture.date_start).getHours() - minHour
 
   return (
-    <>
-      <View
-        style={{
-          position: "absolute",
-          zIndex: 2,
-          backgroundColor: isLight ? "#fff" : palette.darker,
-          height:
-            (open
-              ? LECTURE_HEIGHT_OPEN
-              : props.isSelected
-              ? LECTURE_HEIGHT_COLLAPSED
-              : LECTURE_HEIGHT_COLLAPSED_NOT_SELECTED) +
-            2 * LECTURE_CONTAINER_PADDING,
-          width: timeRange * TIME_SLOT + 2 * LECTURE_CONTAINER_PADDING,
-          left: timeStart * TIME_SLOT + 1 - LECTURE_CONTAINER_PADDING,
-          justifyContent: "center",
-          alignItems: "center",
-          top:
-            props.overlapNumber *
+    <Pressable
+      style={{
+        position: "absolute",
+        zIndex: 3,
+        left: timeStart * TIME_SLOT + ATTACHED_LECTURES_MARGIN + 1,
+        top:
+          props.overlapNumber *
             ((open
               ? LECTURE_HEIGHT_OPEN
-              : props.isSelected
-              ? LECTURE_HEIGHT_COLLAPSED
               : LECTURE_HEIGHT_COLLAPSED_NOT_SELECTED) +
-              2 * LECTURE_CONTAINER_PADDING),
-        }}
-      />
-      <Pressable
-        style={{
-          position: "absolute",
-          zIndex: 3,
-          left: timeStart * TIME_SLOT + ATTACHED_LECTURES_MARGIN + 1,
-          top:
-            props.overlapNumber *
-              ((open
-                ? LECTURE_HEIGHT_OPEN
-                : props.isSelected
-                ? LECTURE_HEIGHT_COLLAPSED
-                : LECTURE_HEIGHT_COLLAPSED_NOT_SELECTED) +
-                2 * LECTURE_CONTAINER_PADDING) +
-            LECTURE_CONTAINER_PADDING,
-          width: timeRange * TIME_SLOT - ATTACHED_LECTURES_MARGIN * 2,
-          height: open
-            ? LECTURE_HEIGHT_OPEN
-            : props.isSelected
-            ? LECTURE_HEIGHT_COLLAPSED
-            : LECTURE_HEIGHT_COLLAPSED_NOT_SELECTED,
-          borderRadius: 18,
-          padding: open ? 8 : 0,
-          backgroundColor: props.isSelected
-            ? props.lecture.lectureColor
-            : isLight
-            ? primary
-            : palette.variant1,
-          justifyContent: "center",
-        }}
-        onPress={() => props.onPress()}
-      >
-        {open ? (
-          <BodyText
-            style={{
-              color: "white",
-              fontSize: 10,
-              fontWeight: "900",
-              alignSelf: "center",
-            }}
-          >
-            {props.lecture.room?.acronym_dn + " - " + props.lecture.title.it}
-          </BodyText>
-        ) : (
-          <BodyText
-            style={{
-              color: "white",
-              fontSize: 10,
-              fontWeight: "900",
-              alignSelf: "center",
-            }}
-          >
-            {props.lecture.room?.acronym_dn}
-          </BodyText>
-        )}
-        {(open || !props.isSelected) && (
-          <View
-            style={{
-              position: "absolute",
-              top: -7,
-              right: -6,
-              width: 20,
-              height: 20,
-              borderRadius: 10,
-              backgroundColor: props.lecture.lectureColor,
-            }}
-          />
-        )}
-      </Pressable>
-    </>
+              2 * LECTURE_CONTAINER_PADDING) +
+          LECTURE_CONTAINER_PADDING -
+          (props.isSelected ? 3 : 0),
+        width: timeRange * TIME_SLOT - ATTACHED_LECTURES_MARGIN * 2,
+        height: open
+          ? LECTURE_HEIGHT_OPEN
+          : props.isSelected
+          ? LECTURE_HEIGHT_COLLAPSED
+          : LECTURE_HEIGHT_COLLAPSED_NOT_SELECTED,
+        borderRadius: 18,
+        padding: open ? 8 : 0,
+        backgroundColor: props.isSelected
+          ? props.lecture.lectureColor
+          : isLight
+          ? primary
+          : palette.variant1,
+        justifyContent: open ? "flex-start" : "center",
+      }}
+      onPress={() => props.onPress()}
+    >
+      {open ? (
+        <BodyText
+          style={{
+            color: "white",
+            fontSize: 10,
+            fontWeight: "900",
+            alignSelf: "center",
+          }}
+        >
+          {props.lecture.room?.acronym_dn + " - " + props.lecture.title.it}
+        </BodyText>
+      ) : (
+        <BodyText
+          style={{
+            color: "white",
+            fontSize: 10,
+            fontWeight: "900",
+            alignSelf: "center",
+          }}
+        >
+          {props.lecture.room?.acronym_dn}
+        </BodyText>
+      )}
+      {(open || !props.isSelected) && (
+        <View
+          style={{
+            position: "absolute",
+            top: -7,
+            right: -6,
+            width: 20,
+            height: 20,
+            borderRadius: 10,
+            backgroundColor: props.lecture.lectureColor,
+          }}
+        />
+      )}
+    </Pressable>
   )
 }
