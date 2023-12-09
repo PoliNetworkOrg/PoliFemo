7b44d0e92d3f187c4e1ed22c1ca4a11bc1fa0e1a
angeousta <132761637+angeousta@users.noreply.github.com>
2023-12-09 15:37:06 +0100

    Update index.tsx and MainStackNavigator.tsx (#22)

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,6 +1,4 @@
 export * from "./MainMenu"
 export * from "./MainTitle"
 export * from "./MenuButton"
-export * from "./StickyHeader"
 export * from "./SearchButton"
-export * from "./PoliSearchBar"
