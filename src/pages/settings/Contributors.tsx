726b35d4536d35e42fd0712241519c0a90087d64
Matteo Luppi <100372313+lups2000@users.noreply.github.com>
2023-12-10 21:39:24 +0100

    create flatlist

--- src/pages/settings/Contributors.tsx
+++ src/pages/settings/Contributors.tsx
@@ -1,11 +1,13 @@
+import { BoxShadowView } from "components/BoxShadow"
 import { NavBar } from "components/NavBar"
-import { BodyText } from "components/Text"
+import { BodyText, Text } from "components/Text"
 import { SettingsStackScreen } from "navigation/NavigationTypes"
-import { View } from "react-native"
-import { usePalette } from "utils/colors"
+import { FlatList, View } from "react-native"
+import { palette, usePalette } from "utils/colors"
 
 export const Contributors: SettingsStackScreen<"Contributors"> = () => {
-  const { homeBackground } = usePalette()
+  const { homeBackground, isLight, primary, background } = usePalette()
+
   return (
     <View
       style={{
@@ -21,8 +23,47 @@ export const Contributors: SettingsStackScreen<"Contributors"> = () => {
           zIndex: 6,
         }}
       >
-        <BodyText>ciao</BodyText>
+        <Text
+          style={{
+            color: isLight ? "#fff" : primary,
+            fontSize: 24,
+            fontWeight: "900",
+          }}
+        >
+          Contributors
+        </Text>
       </View>
+      <BoxShadowView
+        shadow={{
+          color: isLight ? palette.primary : "#000",
+          offset: { y: -8 },
+          opacity: isLight ? 0.1 : 0.45,
+          blur: isLight ? 19 : 32,
+        }}
+        style={{
+          flex: 1,
+          marginTop: 86,
+        }}
+        contentContainerStyle={{
+          backgroundColor: background,
+          borderTopLeftRadius: 30,
+          borderTopRightRadius: 30,
+          overflow: "hidden",
+        }}
+      >
+        <FlatList
+          data={[]}
+          contentContainerStyle={{ paddingBottom: 120, padding: 12 }}
+          renderItem={({ item, index }) => (
+            <View
+              style={{ marginVertical: 6 }}
+              key={`__contributors-item-${index}`}
+            >
+              <BodyText>ciao</BodyText>
+            </View>
+          )}
+        />
+      </BoxShadowView>
       <NavBar />
     </View>
   )
