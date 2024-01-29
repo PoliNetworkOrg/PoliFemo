4b746cef8b57798d343da286bfa30253634813d5
Tommaso Morganti <tommaso.morganti@mail.polimi.it>
2024-01-29 11:11:59 +0100

    fix linting error

--- src/utils/log/logger.ts
+++ src/utils/log/logger.ts
@@ -1,29 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { LogItem } from "./LogItem"
 import { LogLevelsKeys } from "./LogLevels"
 import { getStackTrace } from "./getStackTrace"
 import { logState } from "./logState"
 
-function isString(value: any | undefined | null): boolean {
+function isString(value: any): boolean {
   return typeof value === "string"
 }
 
-export function loggerInfo(o: any | undefined | null) {
+export function loggerInfo(o: any) {
   loggerMain({ o, l: "info" })
 }
 
-export function loggerErr(o: any | undefined | null) {
+export function loggerErr(o: any) {
   loggerMain({ o, l: "error" })
 }
 
-export function loggerDebug(o: any | undefined | null) {
+export function loggerDebug(o: any) {
   loggerMain({ o, l: "debug" })
 }
 
-export function loggerWarn(o: any | undefined | null) {
+export function loggerWarn(o: any) {
   loggerMain({ o, l: "warn" })
 }
 
-function loggerMain({ o, l }: { o: any | undefined | null; l: LogLevelsKeys }) {
+function loggerMain({ o, l }: { o: any; l: LogLevelsKeys }) {
   let n: LogItem
   const stack = getStackTrace()
   if (isString(o)) {
